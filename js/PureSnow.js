1aa04ca6523b59cc184cecf2eeaa658435cb67cb
El Kebabo King <elkebaboking@gmail.com>
2022-02-22 15:00:32 +0100

    Amount of snow flakes change based on window width.

--- js/PureSnow.js
+++ js/PureSnow.js
@@ -14,7 +14,7 @@ window.onload = function(){
       canvas.height = H;
     
       //generate the snowflakes and apply attributes
-      var mf = 100; //max flakes
+      var mf = W/20; //max flakes
       var flakes = [];
     
       //loop throught the empty flakes and apply attributes
