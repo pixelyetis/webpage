6255892d509cf02237df0ae5db9144fd85a01fa1
El Kebabo King <elkebaboking@gmail.com>
2022-03-05 00:12:38 +0100

    Yeti display page now always updates when new wallet connects.

--- js/getYetis.js
+++ js/getYetis.js
@@ -23,24 +23,15 @@ const NETWORK_ID = 5777;	// Truffle chain ID
 // const NETWORK_ID = 4; 		// Rinkeby chain ID
 // const NETWORK_ID = 97; 		// BSC Testnet chain ID
 
-var images = 'sample' 	// Name of images
-const samples = 9		// How many samples are used
-let currentImg = 1
-var stopUpdate = false
 
-function changeImage(){
-	if(stopUpdate) return
-	// Go back to first image when we increment from the last one
-	if(currentImg > samples) currentImg = 1 
+window.ethereum.on('accountsChanged', async function(accounts){
+	updateInfo()
+})
 
-	document.getElementsByClassName("imgs")[0].src = 'images/' + images + currentImg + '.png'
-	document.getElementsByClassName("imgs")[1].src = 'images/' + images + currentImg + '.png'
+async function init(){
+    loginWithEth()
 
-	currentImg++
 }
-
-setInterval(changeImage, 1000)
-
 async function loginWithEth(){
 	if(!window.ethereum){
 		alert('No ETH browser extension detected.')
@@ -55,151 +46,62 @@ async function loginWithEth(){
 		// debugger
 		await ethereum.enable();
 
-		// Hide connect button when successfully connected
-		document.getElementById("walletConnect").classList.add("hidden")
-
-		// Update minting information on the webpage
-		updateInfo()
-
+		// Hide connect button when successfully connected        
+        document.getElementById("walletConnect").style.display = "none"
+        
+        // updateInfo()
+        
 	} catch(error){
-		console.error(error);
+        console.error(error);
+        document.getElementById("walletConnect").style.display = "block"
 	}
-
-
+    
+    
 	// debugger
 }
-
-window.ethereum.on('accountsChanged', async function(accounts){
-	updateInfo()
-})
-
-
-window.ethereum.on('networkChanged', async function(networkId){
-	updateInfo()
-})
-
-async function approveSpending(){
-	const approvalAmount = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
-
-	try{
-		let gasPrice = await web3.eth.getGasPrice() * 2
-		let gasEstimate = await token.methods.approve(yetiAddr, approvalAmount).estimateGas({
-			from: senderAddress
-		})
-		// console.log('Estimated maximum gas price is: ' + web3.utils.fromWei((gasEstimate*gasPrice).toString(), 'ether'));
-
-		let receipt = await token.methods.approve(yetiAddr, approvalAmount).send({
-			from: senderAddress,
-			gas: gasEstimate,
-			gasPrice: gasPrice
-		})
-
-		// console.log('Token allowance: ' + await token.methods.allowance(senderAddress, yetiAddr).call())
-		// console.log('Approval successful!')
-
-		updateInfo()
-
-	}catch(err){
-		console.log('Error approving')
-	}
-}
-
-function decAmount(){
-	var currentAmount = parseInt(document.getElementById("nftAmount").innerHTML)
-
-	if(currentAmount <= 1) return
-	
-	currentAmount--
-	document.getElementById("nftAmount").innerHTML = currentAmount
-	
-	updateInfo()
-}
-
-function addAmount(){
-	var currentAmount = parseInt(document.getElementById("nftAmount").innerHTML)
-
-	if(currentAmount >= 10) return
-	
-	currentAmount++
-	document.getElementById("nftAmount").innerHTML = currentAmount
-
-	updateInfo()
-	
-}
-
 async function updateInfo(){
+    loginWithEth()
+    accounts = await web3.eth.getAccounts();
+    senderAddress = accounts[0]
+    getMyYetis()
 
-	// Guard clause for chain id.
-	if(await web3.eth.net.getId() != NETWORK_ID) {
-
-		document.getElementById("supplyStats").innerHTML = 'Wrong network!'
-
-		document.getElementById("approveButton").style.display = "none"
-		document.getElementById("mintButton").style.display = "none"
-		
-		alert('Wrong network!')
-		return
-	}
-	
-
-	accounts = await web3.eth.getAccounts();
-	senderAddress = accounts[0]
-
-	// Show wallet address
-	const addressCompressed = senderAddress.substring(0,6) + '.....' + senderAddress.substring(senderAddress.length - 4,senderAddress.length)
-	document.getElementById("connectedWalletAddress").innerHTML = addressCompressed
-	
-
-	// Update price for minting an NFT
-	const currentAmount = parseInt(document.getElementById("nftAmount").innerHTML) // Amount of NFTs to purchase
-	const nftPrice = web3.utils.fromWei(await yeti.methods.mintPrice().call(), 'ether') // Price per NFT
-	const tokenSymb = await token.methods.symbol().call()
-	document.getElementById("mintPrice").innerHTML = 'Price: ' + currentAmount*nftPrice + ' ' + tokenSymb
-
-	// Update current and  Maximum supply
-	const currentSupply = await yeti.methods.totalSupply().call() 	// NFTs minted so far
-	const maxSupply = await yeti.methods.MAX_SUPPLY().call()		// NFT supply cap
-	document.getElementById("supplyStats").innerHTML = currentSupply + '/' + maxSupply + ' minted'
-
-	// Update wallet token balance
-	const balance = web3.utils.fromWei(await token.methods.balanceOf(senderAddress.toString()).call(), 'ether')
-	document.getElementById("walletBalance").innerHTML = 'Balance: ' + parseInt(balance) + ' ' + tokenSymb
-
-	// Decide whether to show approve or mint based on whether the wallet can make the purchase or not.
-	const approvalAmount = await token.methods.allowance(senderAddress, yetiAddr).call()
-	// console.log(approvalAmount)
-	// console.log(web3.utils.toWei(nftPrice)*currentAmount)
-	// console.log(approvalAmount <= web3.utils.toWei(nftPrice)*currentAmount)
-	if(approvalAmount >= web3.utils.toWei(nftPrice)*currentAmount){
-		// Approval enough. Show mint button.
-		document.getElementById("buttons").style.display = "none"
-		document.getElementById("mintButton").style.display = "block"
-	}else{
-		// Approval not enough. Show approval button.
-		document.getElementById("approveButton").style.display = "block"
-		document.getElementById("mintButton").style.display = "none"
-	}
 }
+async function getMyYetis(){
+	document.getElementById("allimages").innerHTML = ""
 
-async function mintNFT(){
-	try{
-		const amount = parseInt(document.getElementById("nftAmount").innerHTML)
-		const price = await yeti.methods.mintPrice().call()
-		const total = web3.utils.toHex(amount*price)
+    const yetiAmount = Number(await yeti.methods.balanceOf(senderAddress).call())
+    
+	// Wallet owner has no Pixel Yetis guard clause
+	if(yetiAmount === 0) {
+        var newHeading = document.createElement("p")
+        newHeading.appendChild(document.createTextNode('No Pixel Yetis found for this wallet!'))
+        
+        var newSection = document.createElement("section")
+        newSection.appendChild(newHeading)
+        newSection.classList.add('myYetis')
+        document.getElementById("allimages").appendChild(newSection)
+        
+        console.log('Yeti amount: ' + yetiAmount)
+        return
+    }
+	
+	let imgBase = 'https://pye.fra1.digitaloceanspaces.com/images/'
+	
+	for(let i = 1; i <= await yeti.methods.totalSupply().call(); i++){
+		if(await yeti.methods.ownerOf(i).call() == senderAddress){
+			var pText = document.createElement("h2")
+			pText.appendChild(document.createTextNode('Pixel Yeti #'+i))
+			
+			var newImg = document.createElement("img")
+			newImg.src = imgBase + i + '.png'
 
-		let gasPrice = await web3.eth.getGasPrice() * 2
-		let gasEstimate = await yeti.methods.mintYeti(amount.toString()).estimateGas({from: senderAddress})
-		
-		let receipt = await yeti.methods.mintYeti(amount.toString()).send({
-			from: senderAddress,
-			gas: gasEstimate,
-			gasPrice: gasPrice
-		})
-
-		console.log(receipt)
-		updateInfo()
-
-	}catch(err){
-		console.log(err)
+			var newSection = document.createElement("section")
+			newSection.appendChild(pText)
+			newSection.appendChild(newImg)
+			newSection.classList.add('myYetis')
+			document.getElementById("allimages").appendChild(newSection)
+		}
 	}
+	
+	console.log(document.getElementById("allimages"))
 }
